e2bcc7559f81d1462feff7ea1137306ed3f8fc19
martsin77 <48821995+martsin77@users.noreply.github.com>
2023-02-01 17:38:37 +0200

    lorem images

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,38 @@ window.addEventListener("DOMContentLoaded", (e) => {
   console.log("HI EVERYONE!");
   console.log("====================================");
 
+  let sections = gsap.utils.toArray(".js-section");
+
+  function goToSection(i) {
+    gsap.to(window, {
+      scrollTo: {
+        y: i * innerHeight,
+        autoKill: false,
+        ease: "Power3.easeIn",
+      },
+      duration: 0.1,
+    });
+  }
+
+  ScrollTrigger.defaults({
+    // markers: true,
+  });
+
+  sections.forEach((eachPanel, i) => {
+    const mainAnim = gsap.timeline({ paused: true });
+
+    ScrollTrigger.create({
+      trigger: eachPanel,
+      onEnter: () => goToSection(i),
+    });
+
+    ScrollTrigger.create({
+      trigger: eachPanel,
+      start: "bottom bottom",
+      onEnterBack: () => goToSection(i),
+    });
+  });
+
   //   gsap.fromTo(
   //     ".js-section",
   //     { autoAlpha: 0 },
@@ -12,7 +44,7 @@ window.addEventListener("DOMContentLoaded", (e) => {
   //       stagger: 1,
   //       scrollTrigger: {
   //         trigger: ".trigger",
-  //         pin: true,
+  //         // pin: true,
   //         scrub: true,
   //         // markers: true,
   //         end: window.innerHeight * 4,
@@ -23,6 +55,7 @@ window.addEventListener("DOMContentLoaded", (e) => {
   const initSlider = (sliderClassName) => {
     const swiper = new Swiper(sliderClassName, {
       loop: true,
+      effect: "coverflow",
     });
   };
 

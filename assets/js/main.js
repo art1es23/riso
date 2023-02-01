770d98c41579a902560acacd15912f60b5625a67
martsin77 <48821995+martsin77@users.noreply.github.com>
2023-02-01 17:09:04 +0200

    init sliders and gspa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,32 @@
-document.documentElement.addEventListener("DOMContentLoaded", (e) => {
+window.addEventListener("DOMContentLoaded", (e) => {
   console.log("====================================");
   console.log("HI EVERYONE!");
   console.log("====================================");
+
+  //   gsap.fromTo(
+  //     ".js-section",
+  //     { autoAlpha: 0 },
+  //     {
+  //       duration: 1,
+  //       autoAlpha: 1,
+  //       stagger: 1,
+  //       scrollTrigger: {
+  //         trigger: ".trigger",
+  //         pin: true,
+  //         scrub: true,
+  //         // markers: true,
+  //         end: window.innerHeight * 4,
+  //       },
+  //     }
+  //   );
+
+  const initSlider = (sliderClassName) => {
+    const swiper = new Swiper(sliderClassName, {
+      loop: true,
+    });
+  };
+
+  let arraySliders = [".hero", ".section-1", ".section-2", ".section-3"];
+
+  arraySliders.forEach((slider) => initSlider(slider));
 });

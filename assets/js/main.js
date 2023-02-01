607db6e4bea5de2c2e13c050ad179c1fa1e0a785
martsin77 <48821995+martsin77@users.noreply.github.com>
2023-02-01 15:02:52 +0200

    init default layout of landing page

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+document.documentElement.addEventListener("DOMContentLoaded", (e) => {
+  console.log("====================================");
+  console.log("HI EVERYONE!");
+  console.log("====================================");
+});
